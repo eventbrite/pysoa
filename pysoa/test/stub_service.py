01a347d1820b99f25d5ddaec3236928e93112bf7
Ben Greenberg <bgreenberg@eventbrite.com>
2017-07-21 15:11:24 -0700

    Merge routing functionality into the Client.

--- pysoa/test/stub_service.py
+++ pysoa/test/stub_service.py
@@ -2,15 +2,27 @@ from __future__ import unicode_literals
 
 import re
 
-from pysoa.client import Client
+from pysoa.client.client import (
+    Client,
+    ServiceHandler,
+)
+from pysoa.client.settings import ClientSettings
 from pysoa.common.types import Error
 from pysoa.common.transport.local import LocalClientTransport
-from pysoa.common.serializer.msgpack_serializer import MsgpackSerializer
 from pysoa.server import Server
 from pysoa.server.action import (
     Action,
     ActionError,
 )
+
+
+def _make_stub_action(action_name, body={}, errors=[]):
+    action_class_name = ''.join([part.capitalize() for part in re.split(r'[^a-zA-Z0-9]+', action_name)])
+    return type(
+        str(action_class_name),
+        (StubAction,),
+        dict(body=body, errors=errors),
+    )
 
 
 class StubAction(Action):
@@ -34,6 +46,14 @@ class StubAction(Action):
             return self.body
 
 
+class StubClientSettings(ClientSettings):
+    defaults = {
+        'transport': {
+            'path': 'pysoa.test.stub_service:StubClientTransport'
+        }
+    }
+
+
 class StubClient(Client):
     """
     A Client for testing code that calls service actions.
@@ -43,20 +63,52 @@ class StubClient(Client):
     test against a genuine service in a unit testing environment.
     """
 
-    def __init__(self, service_name='test', action_class_map=None, **kwargs):
-        transport = StubClientTransport(service_name=service_name, action_class_map=action_class_map)
-        serializer = MsgpackSerializer()
-        super(StubClient, self).__init__(service_name, transport, serializer)
+    settings_class = StubClientSettings
 
-    def stub_action(self, action, body=None, errors=None):
-        self.transport.stub_action(action, body=body, errors=errors)
+    def __init__(self, service_action_map=None, **kwargs):
+        """
+        Generate settings based on a mapping of service names to actions.
+
+        Args:
+            service_action_map: dict of {service_name: <action map>}
+        """
+        service_action_map = service_action_map or {}
+        config = {}
+        for service_name, action_map in service_action_map.items():
+            config[service_name] = {
+                'transport': {
+                    'kwargs': {
+                        'action_map': action_map,
+                    }
+                }
+            }
+        super(StubClient, self).__init__(config)
+
+    def stub_action(self, service_name, action, body=None, errors=None):
+        if service_name not in self.handlers:
+            self.handlers[service_name] = ServiceHandler(service_name, self.settings_class({}))
+        self.handlers[service_name].transport.stub_action(action, body=body, errors=errors)
 
 
 class StubClientTransport(LocalClientTransport):
-    """A transport that incorporates an automatically-configured server for handling requests."""
+    """A transport that incorporates an automatically-configured Server for handling requests."""
 
-    def __init__(self, service_name='test', action_class_map=None):
-        action_class_map = action_class_map or {}
+    def __init__(self, service_name='test', action_map=None):
+        """
+        Configure a StubServer to handle requests. Creates a new subclass of StubServer using the service name and action
+        mapping provided.
+
+        Args:
+            service_name: string
+            action_map: dict of {action_name: {'body': action_body, 'errors': action_errors}} where action_body is a
+                dict and action_errors is a list
+        """
+        action_map = action_map or {}
+        # Build the action_class_map property for the new Server class
+        action_class_map = {
+            name: _make_stub_action(name, a.get('body', {}), a.get('errors', [])) for name, a in action_map.items()
+        }
+        # Create the new Server subclass
         server_class_name = ''.join([part.capitalize() for part in re.split(r'[^a-zA-Z0-9]+', service_name)]) + 'Server'
         server_class = type(
             str(server_class_name),
@@ -79,10 +131,4 @@ class StubServer(Server):
         The name of the action class is the action name converted to camel case. For example, an action
         called 'update_foo' will have an action class called UpdateFoo.
         """
-        action_class_name = ''.join([part.capitalize() for part in re.split(r'[^a-zA-Z0-9]+', action)])
-        new_action_class = type(
-            str(action_class_name),
-            (StubAction,),
-            dict(body=body, errors=errors),
-        )
-        self.action_class_map[action] = new_action_class
+        self.action_class_map[action] = _make_stub_action(action, body, errors)
--- tests/common_tests/test_settings.py
+++ tests/common_tests/test_settings.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
 import pytest
-import six
 
 from pysoa.common.settings import (
     Settings,
