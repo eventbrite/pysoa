a80d0a35165670d73cdba9512db43e880c3d288a
Nick Williams <nwilliams@eventbrite.com>
2018-02-06 14:18:44 -0600

    Released PySOA version 0.27.0
    Changelog Details:
    - Bump Conformity and remove duplicate msgpack-python dependency
    - Add support for auto-reloading code changes in dev environments
    - Use Invoke Release for releases going forward
    - Fix bug causing response mix-ups with transport cache
    - Add ability to fork multiple server processes with the standalone command
    - Start request counter at a random value (#50)
    - Add .pytest_cache to .gitignore
    - Remove mock of randint
    - Improve status action to enable abbreviated responses when only the version is needed
    - Tweak comment

--- pysoa/version.py
+++ pysoa/version.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
 
 
-__version_info__ = (0, 26, 1)
-__version__ = '.'.join(map(str, __version_info__))
+__version_info__ = (0, 27, 0)
+__version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
