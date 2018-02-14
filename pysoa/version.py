74b13b5583fca676af8ed1e125c2ef90893ac0ff
Nick Williams <nwilliams@eventbrite.com>
2018-02-14 15:20:20 -0600

    Released PySOA version 0.29.0
    Changelog Details:
    - Bump Conformity
    - Add support for controlling request log logging level
    - Add support for clean-up operations before and after requests

--- pysoa/version.py
+++ pysoa/version.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
 
 
-__version_info__ = (0, 28, 1)
+__version_info__ = (0, 29, 0)
 __version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
