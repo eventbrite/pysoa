2a74267978b6bdbf05c47cb11afaa3757d4c3095
yan guo <yan.guo@eventbrite.com>
2018-03-13 12:31:55 -0700

    Released PySOA version 0.32.1.
    Changelog Details:
    - Re-raise InvalidExpansionKey for expansion exception when request has invalid key

--- pysoa/version.py
+++ pysoa/version.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
 
 
-__version_info__ = (0, 32, 0)
-__version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
+__version_info__ = (0, 32, 1)
+__version__ = '.'.join(map(str, __version_info__))
