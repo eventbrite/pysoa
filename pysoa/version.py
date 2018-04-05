a41eb2f42aca0faf5218150c8d6aca1b28250460
Nick Williams <nwilliams@eventbrite.com>
2018-04-05 16:32:24 -0500

    Released PySOA version 0.35.0
    Changelog Details:
    - [MINOR] Add stock ability to include other services' status in status
    - [MAJOR] Add support for setting a custom timeout when sending a request

--- pysoa/version.py
+++ pysoa/version.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
 
 
-__version_info__ = (0, 34, 0)
+__version_info__ = (0, 35, 0)
 __version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
