bb51a6a902841155f141d5be7e6a0ae068d8e5a4
Nick Williams <nwilliams@eventbrite.com>
2019-09-26 11:23:16 -0500

    Released PySOA version 0.70.0
    Changelog Details:
    - [MINOR] #204: Add is_caller_error attribute to Error objects

--- pysoa/version.py
+++ pysoa/version.py
@@ -4,5 +4,5 @@ from __future__ import (
 )
 
 
-__version_info__ = (0, 69, 1)
+__version_info__ = (0, 70, 0)
 __version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
