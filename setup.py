1a9c582844dbcd62187ad474c22b209f103d66c5
Riley Mathews <rmathews@eventbrite.com>
2023-01-04 11:31:45 -0600

    revert asyncio chagne

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ test_plan_requirements = test_helper_requirements + [
     'pyparsing~=2.2',
     'pytest>4.2,<5.4',
     'Faker>15.3.4;python_version>"3.6"'
-    'pytest-asyncio~=0.20.0;python_version>"3.4"',
+    'pytest-asyncio~=0.10.0;python_version>"3.4"',
 ]
 
 mypy_require = [
