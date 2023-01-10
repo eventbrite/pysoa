a5040aa386d7385a372066e2d73df116041e07f4
Riley Mathews <rmathews@eventbrite.com>
2023-01-10 16:06:53 -0600

    bump typing extensions for >python37

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ install_requires = [
     'redis>=2.10,<4.0,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',  # shortest way to say 2.10+ or 3.4+ but not older versions
     'six~=1.10',
     'typing~=3.7.4;python_version<"3.5"',
-    'typing-extensions~=3.7.4;python_version<"3.8"',
+    'typing-extensions~=3.7.4;python_version<"3.7"',
+    'typing-extensions~=3.10;python_version>="3.7"',
 
     # For context, see the comment in pysoa.common.compatibility. Due to the peculiarities of the patching detailed
     # there, we pin these dependencies to hard versions, or else things might break when they update. When new versions
